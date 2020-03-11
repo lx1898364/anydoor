d161d6627e196ba2735ab51e74673e7848da0eeb
liuxin <liuxin1@xiaozhu.com>
2020-03-11 11:12:47 +0800

    文件压缩功能修复

--- src/helper/compress.js
+++ src/helper/compress.js
@@ -2,10 +2,10 @@ const {createGzip, createDeflate} = require('zlib');
 
 module.exports = (rs, req, res) => {
     const acceptEncoding = req.headers['accept-encoding'];
-    if (!acceptEncoding || acceptEncoding.match(/\b(gzip|deflate)|b/)) {
+    if (!acceptEncoding || !acceptEncoding.match(/\b(gzip|deflate)|b/)) {
         return rs;
     } else if(acceptEncoding.match(/\bgzip\b/)){
-        res.setHeader('Content-Encoding', 'default');
+        res.setHeader('Content-Encoding', 'gzip');
         return rs.pipe(createGzip());
     } else if(acceptEncoding.match(/\bdefault\b/)) {
         res.setHeader('Content-Encoding', 'default');
